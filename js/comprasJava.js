ac3b466584dd6c4c56c998dc057900876d3bfc22
Rodrigo Suarez <ing.rsuarez@gmail.com>
2022-07-22 12:28:25 -0300

    Url java test

--- js/comprasJava.js
+++ js/comprasJava.js
@@ -1,16 +1,21 @@
 
 const userCheck = document.getElementById("userCheck");
-
+var base_url=window.location.host;
 
 // FILTRO PEDIDOS POR USUARIO
 
 userCheck.addEventListener("change", function (evt) {
 	evt.preventDefault();
-	if (window.location.href == "http://[::1]/ipac-mvc/index.php/compras/mis_pedidos"){
-		window.location.href = "http://[::1]/ipac-mvc/index.php/compras/pedidos";	
-	}else{
-		window.location.href = "http://[::1]/ipac-mvc/index.php/compras/mis_pedidos";
-		
-	}
+	var url = window.location.href;
+	var urlC = url.split("/")
+
+	alert("urlC"+urlC[6]+" url: "+url);
+	// if (window.location.href == "http://[::1]/ipac-mvc/index.php/compras/mis_pedidos"){
+	// 	alert(base_url+"/ipac-mvc/index.php/compras/pedidos")
+	// 	// window.location.href = base_url+"/ipac-mvc/index.php/compras/pedidos";	
+	// }else{
+	// 	// window.location.href = base_url+"/ipac-mvc/index.php/compras/mis_pedidos";
+	// 	alert(base_url+"/ipac-mvc/index.php/compras/pedidos")
+	// }
 })
 
