4bcbf023a4156db08247d7a133a3867d2b41ee8a
Rodrigo Suarez <ing.rsuarez@gmail.com>
2022-07-22 14:30:51 -0300

    Url java test

--- js/comprasJava.js
+++ js/comprasJava.js
@@ -8,8 +8,9 @@ userCheck.addEventListener("change", function (evt) {
 	evt.preventDefault();
 	var url = window.location.href;
 	var urlC = url.split("/")
+	var largo = urlC.length-2;
 
-	alert("urlC: "+urlC[0]+" url: "+url+" rulpath: "+window.location.pathname);
+	alert("urlC: "+urlC[largo]+"   url: "+url+" rulpath: "+window.location.pathname);
 	// if (window.location.href == "http://[::1]/ipac-mvc/index.php/compras/mis_pedidos"){
 	// 	alert(base_url+"/ipac-mvc/index.php/compras/pedidos")
 	// 	// window.location.href = base_url+"/ipac-mvc/index.php/compras/pedidos";	
