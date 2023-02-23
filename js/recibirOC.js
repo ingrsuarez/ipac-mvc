d4d51e4011a56b9d8b6d9fd4433653bfda905cf0
rsuarez <ing.rsuarez@gmail.com>
2023-02-23 11:26:42 -0300

    Numpad Fix

--- js/recibirOC.js
+++ js/recibirOC.js
@@ -1,13 +1,18 @@
 var firstNumber = document.getElementById('firstNumber');
 var lastNumber = document.getElementById('lastNumber');
 
+// firstNumber.addEventListener("keydown",function(evt){
+
+// })
+
 //First number zero complete function
 firstNumber.addEventListener("keypress", function (evt) {
 	var key = event.key;
 	var code=event.code;
-	let type = code.match("Digit");
-
-	if (type != null){
+	let type1 = code.match("Digit");
+	let type2 = code.match("Numpad");
+	console.log("key: "+key+" code: "+code)
+	if ((type1 != null) || (type2 != null)){
 		// alert("Hubo ingreso: "+typeof(key)+" tipo: "+code);
 
 	}else{
@@ -30,9 +35,10 @@ firstNumber.addEventListener("focusout",function(evt){
 lastNumber.addEventListener("keypress", function (evt) {
 	var key = event.key;
 	var code=event.code;
-	let type = code.match("Digit");
+	let type1 = code.match("Digit");
+	let type2 = code.match("Numpad");
 
-	if (type != null){
+	if ((type1 != null) || (type2 != null)){
 		// alert("Hubo ingreso: "+typeof(key)+" tipo: "+code);
 
 	}else{
