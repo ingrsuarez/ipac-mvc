7b791c047024bb8cba61fee1099dc49c58410f27
rsuarez <ing.rsuarez@gmail.com>
2023-06-20 22:17:06 -0300

    Fix FPDF line 1212 escape s

--- FPDF/fpdf.php
+++ FPDF/fpdf.php
@@ -1209,7 +1209,7 @@ protected function _UTF8toUTF16($s)
 protected function _escape($s)
 {
 	// Escape special characters
-	if(strpos($s,'(')!==false || strpos($s,')')!==false || strpos($s,'\\')!==false || strpos($s,"\r")!==false)
+	if(($s != null) AND (strpos($s,'(')!==false || strpos($s,')')!==false || strpos($s,'\\')!==false || strpos($s,"\r")!==false))
 		return str_replace(array('\\','(',')',"\r"), array('\\\\','\\(','\\)','\\r'), $s);
 	else
 		return $s;
